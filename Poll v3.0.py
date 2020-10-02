87d5ba167edac0da91ade2a908983cfa723a407f
yashpawar394 <71833743+yashpawar394@users.noreply.github.com>
2020-10-02 10:36:18 +0530

    Fixed tkinter tcl_error and added mainloop.
    Due to pack (),  line 9 and 10 was throwing tcl_error.
    You can't use pack() and grid() together.
    And added mainloop, so the code will be visible on screen.

--- Poll v3.0.py
+++ Poll v3.0.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 #save to GitHub
 root = Tk()
@@ -5,8 +6,10 @@ root = Tk()
 canvas = Canvas(root, width=400, height=400)
 canvas.pack()
 
-Label(root, text="First Name").grid(row=0)
-Label(root, text="How many Siblings?").grid(row=1)
+Label(root, text="First Name").pack()
+Label(root, text="How many Siblings?").pack()
 
 name = Entry(root)
 SibNum = Entry(root)
+
+root.mainloop()
